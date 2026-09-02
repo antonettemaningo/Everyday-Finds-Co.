95b3fd7f20312bca0f3ad32b51415a4385e22477
antonettemaningo <antonettemaningo1028@gmail.com>
2026-09-02 22:06:56 +0800

    Delete Everyday-Finds-Co-V3-Fixed directory

--- Everyday-Finds-Co-V3-Fixed/script.js
+++
@@ -1,5 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{
- const menu=document.querySelector('.menu'), links=document.querySelector('.navlinks');
- if(menu) menu.addEventListener('click',()=>links.classList.toggle('mobile-open'));
-});
